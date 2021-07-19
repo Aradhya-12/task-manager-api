080da9460a4d66df311e9b1ff53673459be59b04
Aradhya Tripathi <arutripathi011@gmail.com>
2021-07-20 00:57:39 +0530

    databasechanges

--- src/mongoose.js
+++ src/mongoose.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 //as monggose is connecion btw node and our mongodb it's methods are much silimar to mongodb
 
 //connect database (task-manager-api) and our localhost
+console.log(process.env.MONGO1_URL)
 mongoose.connect(process.env.MONGO1_URL,{
     useNewUrlParser: true,
+    useUnifiedTopology: true ,
     useCreateIndex : true // tell mongoose to create indices for collections to easily access its doc
 })
 
